8fc134185f398498cffff0fae3d210cd1ff725fa
aamirkhan06 <114560386+aamirkhan06@users.noreply.github.com>
2024-06-18 22:05:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ function fibonacci(num) {
 // your code here
 	let a = 0, b = 1, temp;
 
-  if (num === 0) return 0; // The first Fibonacci number is 0
-  if (num === 1) return 1; // The second Fibonacci number is 1
+  if (num === 1) return 0; // The first Fibonacci number is 0
+  if (num === 2) return 1; // The second Fibonacci number is 1
 
-  for (let i = 2; i <= num; i++) {
+  for (let i = 3; i <= num; i++) {
     temp = a + b;
     a = b;
     b = temp;
